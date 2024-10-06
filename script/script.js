222d314260aeae1aac0967f4b06020001b24d9ec
Nayan Suter <nayankbps@gmail.com>
2024-10-06 20:40:27 +0600

    category section added

--- script/script.js
+++ script/script.js
@@ -1,1 +1,20 @@
-// alert('hello')
+const loadCategory=()=>{
+    const url='https://openapi.programming-hero.com/api/phero-tube/categories';
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>showCategories(data.categories))
+}
+
+const showCategories=(categories)=>{
+const categoryContainer= document.getElementById('category');
+categories.forEach(ct=>{
+   const button= document.createElement('button');
+   button.classList='btn bg-teal-200 border-none mx-4 py-0 text-base '
+   button.innerText=ct.category;
+   categoryContainer.appendChild(button)
+
+
+})
+}
+
+loadCategory()
